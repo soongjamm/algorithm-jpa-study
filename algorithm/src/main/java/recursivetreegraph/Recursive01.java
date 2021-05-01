5963a9736598224e15889a7e80851cdabf01027c
soongjamm <kimstz0303@gmail.com>
2021-05-01 11:50:34 +0900

    recursive 1~3

---
+++ algorithm/src/main/java/recursivetreegraph/Recursive01.java
@@ -0,0 +1,39 @@
+package recursivetreegraph;
+
+/**
+ * 재귀함수
+ * <p>
+ * 자연수 N이 입력되면 재귀함수를 이용하여 1부터 N까지를 출력하는 프로그램을 작성하세요.
+ * <p>
+ * ▣ 입력설명
+ * 첫 번째 줄은 정수 N(3<=N<=10)이 입력된다.
+ * <p>
+ * ▣ 출력설명
+ * 첫째 줄에 출력한다.
+ * <p>
+ * ▣ 입력예제 1
+ * 3
+ * <p>
+ * ▣ 출력예제 1
+ * 1 2 3
+ */
+
+public class Recursive01 {
+    public void solution(int n) {
+        dfs(n);
+        System.out.println();
+    }
+
+    public void dfs(int n) {
+        if (n <= 0) {
+            return;
+        }
+        dfs(n-1);
+        System.out.print(n + " ");
+    }
+
+    public static void main(String[] args) {
+        Recursive01 recursive = new Recursive01();
+        recursive.solution(3);
+    }
+}
---
+++ algorithm/src/main/java/recursivetreegraph/RecursiveTree03.java
@@ -0,0 +1,78 @@
+package recursivetreegraph;
+
+/**
+ * Recursive02이진트리 순회(깊이우선탐색)
+ * 아래 그림과 같은 이진트리를 전위순회와 후위순회를 연습해보세요.
+ * 1
+ * 2       3
+ * 4 5     6 7
+ * 전위순회 출력 : 1 2 4 5 3 6 7
+ * 중위순회 출력 : 4 2 5 1 6 3 7
+ * 후위순회 출력 : 4 5 2 6 7 3 1
+ */
+
+public class RecursiveTree03 {
+
+    static class Node {
+        private int data;
+        private Node left;
+        private Node right;
+
+        public Node(int data) {
+            this.data = data;
+        }
+    }
+
+    public void solution() {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        preorder(root);
+        System.out.println();
+        inorder(root);
+        System.out.println();
+        postorder(root);
+        System.out.println();
+    }
+
+    private void print(Node node) {
+        System.out.print(node.data + " ");
+    }
+
+    public void preorder(Node node) {
+        if(node == null) {
+            return;
+        }
+        print(node);
+        preorder(node.left);
+        preorder(node.right);
+    }
+
+    public void inorder(Node node) {
+        if(node == null) {
+            return;
+        }
+        inorder(node.left);
+        print(node);
+        inorder(node.right);
+    }
+
+    public void postorder(Node node) {
+        if(node == null) {
+            return;
+        }
+        postorder(node.left);
+        postorder(node.right);
+        print(node);
+    }
+
+    public static void main(String[] args) {
+        RecursiveTree03 recursive = new RecursiveTree03();
+        recursive.solution();
+    }
+}
